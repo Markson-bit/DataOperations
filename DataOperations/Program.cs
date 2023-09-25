ac364f4d65856f4bb49b7a13c3438e8eb2de6285
Marek Małolepszy <marek4924@gmail.com>
2023-09-25 19:40:01 +0200

    Add project files.

---
+++ DataOperations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DataOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>();
+
+            foreach (var a in list)
+            { 
+
+            }
+
+
+        }
+
+   
+    }
+}
